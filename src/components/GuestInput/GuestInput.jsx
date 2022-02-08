d6bd6720d6943513f7abb44c05973ece79d9b427
mira-kine <mirak0419@gmail.com>
2022-02-07 17:18:13 -0800

    created guest form, update list function

--- src/components/GuestInput/GuestInput.jsx
+++ src/components/GuestInput/GuestInput.jsx
@@ -1,5 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
+import { useGuest } from '../../context/GuestProvider/GuestProvider';
+import { useEntry } from '../../context/EntryProvider/EntryProvider';
 
 export default function GuestInput() {
-  return <div></div>;
+  const [name, setName] = useState('');
+  const [guestEntry, setGuestEntry] = useState('');
+  const { guest, setGuest } = useGuest();
+  const { entry, setEntry } = useEntry();
+
+  function updateList() {
+    // if there is no guest entry that exists, set new Guest and entry
+    if (!guestEntry) return setGuest(name);
+    // spread current entries available out, add the new one
+    setEntry([entry, { name, guestEntry }]);
+    // reset entry space as empty string
+    setGuestEntry('');
+  }
+
+  // handle for submit button
+  const handleSubmit = (e) => {
+    e.preventDefault;
+    updateList();
+  };
+
+  // set
+  return (
+    <div>
+      <form>
+        <div>
+          <label>Guest Name: </label>
+          <input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label>Guest Entry: </label>
+          <textarea
+            placeholder="type something nice here"
+            value={guestEntry}
+            onChange={(e) => setGuestEntry(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
+  );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import EntryList from '../../components/EntryList/EntryList';
+import { EntryProvider } from '../../context/EntryProvider/EntryProvider';
+import GuestInput from '../../components/GuestInput/GuestInput';
+import Header from '../../components/Header/Header';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <EntryProvider>
+        <Header />
+        <GuestInput />
+        <EntryList />
+      </EntryProvider>
+    </div>
+  );
 }
